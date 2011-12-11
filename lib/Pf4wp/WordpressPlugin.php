dea4f0dc23bfeaf616fc7f368c1e4213212b8506
Michael Green <mgreen@emixode.com>
2011-12-11 01:45:03 +0000

    Merge development (PostMetaBox update)

--- lib/Pf4wp/WordpressPlugin.php
+++ lib/Pf4wp/WordpressPlugin.php
@@ -32,7 +32,7 @@ use Pf4wp\Template\TwigEngine;
  * WordPress: 3.1.0
  *
  * @author Mike Green <myatus@gmail.com>
- * @version 0.9.2
+ * @version 0.9.3
  * @package Pf4wp
  */
 class WordpressPlugin
