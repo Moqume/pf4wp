d7b605646d93d50810630e4413b8c5f0b0fce6f1
Michael Green <mgreen@emixode.com>
2012-01-05 18:34:21 +0000

    Merged Development. Prep tag 0.9.6.

--- lib/Pf4wp/WordpressPlugin.php
+++ lib/Pf4wp/WordpressPlugin.php
@@ -32,7 +32,7 @@ use Pf4wp\Template\TwigEngine;
  * WordPress: 3.1.0
  *
  * @author Mike Green <myatus@gmail.com>
- * @version 0.9.5
+ * @version 0.9.6
  * @package Pf4wp
  */
 class WordpressPlugin
