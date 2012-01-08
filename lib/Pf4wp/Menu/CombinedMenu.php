ec2870c476bacb96f760ee496b83b610396e2722
Michael Green <mgreen@emixode.com>
2012-01-08 19:58:29 +0000

    Updated copyright year

--- lib/Pf4wp/Menu/CombinedMenu.php
+++ lib/Pf4wp/Menu/CombinedMenu.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2011 Mike Green <myatus@gmail.com>
+ * Copyright (c) 2011-2012 Mike Green <myatus@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
