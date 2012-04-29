b6cb5dc40a3cac2dc887e09813ce8eee61578cad
Michael Green <myatus@gmail.com>
2012-04-29 15:19:11 +0100

    Upped version

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -5,19 +5,19 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 /* PLEASE DO NOT DELETE THE FOLLOWING LINE AND LEAVE IT AT THE TOP: */
 if (!isset($_pf4wp_file) || array_shift(explode('/', str_replace('\\', '/', plugin_basename(dirname(__FILE__))))) != plugin_basename(dirname($_pf4wp_file))) die('Restricted Access');
 
 /**
  * This include file does a version check and creates an UCL instance.
  *
- * If it fails either the WordPress or PHP checks, it will deactivate the 
- * plugin and gracefully "die" if the end user is within the WordPress 
+ * If it fails either the WordPress or PHP checks, it will deactivate the
+ * plugin and gracefully "die" if the end user is within the WordPress
  * Admin/Dashboard area, or silently return otherwise.
  *
- * If the checks pass, it will set the variable $_pf4wp_check_pass 
- * to `true` and will set a variable $_pf4wp_ucl containing the UCL 
+ * If the checks pass, it will set the variable $_pf4wp_check_pass
+ * to `true` and will set a variable $_pf4wp_ucl containing the UCL
  * (UniversalClassLoader) instance.
  *
  * Before calling this include file, please set the variable $_pf4wp_file
@@ -44,29 +44,29 @@ if (!isset($_pf4wp_version_check_php))
 // Default mininum WordPress version
 if (!isset($_pf4wp_version_check_wp))
     $_pf4wp_version_check_wp = '3.1.0';
-    
+
 $_pf4wp_plugin_name = '';
 
 if (is_admin() && function_exists('get_plugin_data')) {
     $_pf4wp_plugin_data = get_plugin_data($_pf4wp_file);
-    
+
     if (isset($_pf4wp_plugin_data['Name']))
         $_pf4wp_plugin_name = '"' . $_pf4wp_plugin_data['Name'] . '"'; // Override with actual name
-        
+
     unset($_pf4wp_plugin_data);
 }
 
 /* Version Check */
 
-if (($_pf4wp_old_php = version_compare(PHP_VERSION, $_pf4wp_version_check_php, '<')) || 
+if (($_pf4wp_old_php = version_compare(PHP_VERSION, $_pf4wp_version_check_php, '<')) ||
     version_compare($wp_version, $_pf4wp_version_check_wp, '<')) {
     if (is_admin()) {
         $_pf4wp_deactivated = '';
-        
+
         // Attempt to load some plugin helpers provided by WordPress
         if (!function_exists('deactivate_plugins'))
             @include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-        
+
         // De-activate the plugin, if possible
         if (function_exists('deactivate_plugins')) {
             deactivate_plugins($_pf4wp_file, true);
@@ -98,11 +98,11 @@ if (!class_exists($_pf4wp_ucl_class))
     require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php'; // Always include default UCL
 
 if (extension_loaded('apc')) {
-    if (!defined('PF4WP_APC')) 
+    if (!defined('PF4WP_APC'))
         define('PF4WP_APC', true);
-        
+
     $_pf4wp_ucl_class = 'Symfony\\Component\\ClassLoader\\ApcUniversalClassLoader';
-    
+
     if (!class_exists($_pf4wp_ucl_class))
         require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';
 
@@ -112,7 +112,7 @@ if (extension_loaded('apc')) {
     if (!defined('PF4WP_APC'))
         define('PF4WP_APC', false);
 
-    if (class_exists($_pf4wp_ucl_class))    
+    if (class_exists($_pf4wp_ucl_class))
         $_pf4wp_ucl = new $_pf4wp_ucl_class();
 }
 
@@ -131,4 +131,4 @@ $_pf4wp_check_pass = true;
 
 // Set the version
 if (!defined('PF4WP_VERSION'))
-    define('PF4WP_VERSION', '1.0.6.1');
+    define('PF4WP_VERSION', '1.0.7');
